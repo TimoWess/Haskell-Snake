d5d7f417a4c3d1969bf18a4d8cbcc99a7cca8e95
Timo Wesselmann <timo.wesselmann@gmail.com>
2022-09-09 02:44:00 +0200

    uodate: check for loosing condition

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
-import Data.List.Split
+import Data.List.Split (chunksOf)
 
 main :: IO ()
 main = do
     let board = initBoard gWidth gHeight
         snake = initSnake
-    update board snake
+    update (board, snake)
 
 gWidth :: Int
 gWidth = 20
@@ -41,22 +41,28 @@ data Space
     = Empty
     | Food
     | Body
+    | Head
+    deriving (Eq)
 
 instance Show Space where
     show Empty = " "
     show Food = "*"
-    show Body = "#"
+    show Body = "+"
+    show Head = "#"
+
+type Game = (Board, Snake)
 
 initBoard :: Int -> Int -> Board
 initBoard width height =
-    Board (width, height, (replicate (width * height) Empty))
+    Board
+        (width, height, replaceNth 6 Food ((replicate (width * height) Empty)))
 
 initSnake :: Snake
 initSnake = (R, [(2, 0), (1, 0), (0, 0)])
 
 positionSnakeOnBoard :: Board -> Snake -> Board
 positionSnakeOnBoard board@(Board (width, height, spaces)) snake@(_, parts) =
-    let lst = snakePositions board snake
+    let lst = snakePositionsMap board snake
      in Board
             ( width
             , height
@@ -64,8 +70,11 @@ positionSnakeOnBoard board@(Board (width, height, spaces)) snake@(_, parts) =
                   (\(space, index) akk ->
                        let isSnake = lookup index lst
                         in (if maybe False id isSnake
-                                then Body
-                                else Empty) :
+                                then if index ==
+                                        (\(x, y) -> x + y * width) (head parts)
+                                         then Head
+                                         else Body
+                                else space) :
                            akk)
                   []
                   (zip spaces [0 ..]))
@@ -78,16 +87,17 @@ directionToMove dir =
         D -> (0, 1)
         L -> (-1, 0)
 
-applyMove :: Direction -> Part -> Part
-applyMove dir (x, y) = (x + dx, y + dy)
+applyMove :: Board -> Direction -> Part -> Part
+applyMove board@(Board (width, height, _)) dir (x, y) = (x + dx, y + dy)
   where
     (dx, dy) = directionToMove dir
 
-updateSnake :: Snake -> Snake
-updateSnake (dir, body) = (dir, (applyMove dir (head body)) : (init body))
+updateSnake :: Game -> Snake
+updateSnake (board, (dir, body)) =
+    (dir, (applyMove board dir (head body)) : (init body))
 
-growSnake :: Snake -> Snake
-growSnake (dir, body) = (dir, (applyMove dir (head body)) : body)
+growSnake :: Game -> Snake
+growSnake (board, (dir, body)) = (dir, (applyMove board dir (head body)) : body)
 
 changeDirection :: Snake -> Int -> Snake
 changeDirection (currentDir, body) change =
@@ -105,11 +115,27 @@ replaceNth n newVal (x:xs)
     | n == 0 = newVal : xs
     | otherwise = x : replaceNth (n - 1) newVal xs
 
-snakePositions :: Board -> Snake -> [(Int, Bool)]
-snakePositions (Board (width, height, _)) (_, body) =
+partPosition :: Part -> Int
+partPosition (x, y) = x + y * gWidth
+
+snakePositionsMap :: Board -> Snake -> [(Int, Bool)]
+snakePositionsMap (Board (width, height, _)) (_, body) =
     map (\(x, y) -> (x + y * width, True)) body
 
-update :: Board -> Snake -> IO ()
-update board snake = do
+update :: Game -> IO ()
+update (board@(Board (width, height, spaces)), snake) = do
     print $ positionSnakeOnBoard board snake
-    update board (updateSnake snake)
+    let newSnake@(dir, snakeHead@(x, y):body) = updateSnake (board, snake)
+        headPosition = partPosition snakeHead
+    if x < 0 || y < 0 || x >= width || y >= height
+        then fail "Snake hit wall"
+        else if (spaces !! headPosition) == Body
+                 then fail "Snake hit body"
+                 else if (spaces !! headPosition) == Food
+                          then update
+                                   ( Board
+                                         ( width
+                                         , height
+                                         , replaceNth headPosition Empty spaces)
+                                   , (growSnake (board, snake)))
+                          else update (board, (newSnake))
